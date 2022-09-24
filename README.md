322ae54eceba8cae857da8838ec1d19f071bdb40
breakid <>
2022-09-24 14:39:41 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# typescript_cleaner
+# typescript_cleaner.py
+
+A simple utility to clean shell typescript files by removing terminal escape sequences and (optionally) applying backspace characters.
+
+Typescript files (created using the `script` command) contain escape sequences for backspaces, font colors, tab completion, etc., which make them difficult to read (without using `cat`) or grep through. This utility aims to fix that. Just provide one or more typescript filepaths as an argument, and the script will automatically remove escape sequences (optionally leaving BELL and/or backspace characters) and output the result to a matching file with '_cleaned' appended to the filename.
+
+Requires Python 3.6+ due to use of type hinting.
+
+
+## Usage
+
+---
+    python3 typescript_cleaner.py [-h] [--keep_backspace] [--keep_bell] [filepaths ...]
+
+    Cleans shell typescript files by removing terminal escape sequences and applying backspace characters
+
+    positional arguments:
+      filepaths         Paths to typescript files (i.e., logs created using the "script" command)
+
+    optional arguments:
+      -h, --help        show this help message and exit
+      --keep_backspace  Keep BACKSPACE characters; useful for identifying things typed accidentally (like passwords)
+      --keep_bell       Keep BELL characters; useful for identifying tab completion
+
+
+## Examples
+
+---
+    # Create a typescript
+    script -f ssh_session_$(date +%F).log
+    
+    # Remove all escape sequences and metacharacters
+    python3 typescript_cleaner.py ssh_session_2022-09-24.log
+    
+    # Keep BELL characters (to show tab completion)
+    python3 typescript_cleaner.py -keep_bell ssh_session_2022-09-24.log
+    
+    # Keep BACKSPACE characters
+    python3 typescript_cleaner.py -keep_backspace ssh_session_2022-09-24.log
+    
+    # Keep BACKSPACE and BELL characters
+    python3 typescript_cleaner.py -keep_backspace -keep_bell ssh_session_2022-09-24.log
---
+++ typescript_cleaner.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import os
+import re
+import sys
+from argparse import ArgumentParser
+
+# Match character sequence between the ESC character (\x1b) and the BELL character (\x07)
+# Source: https://superuser.com/questions/236930/how-to-clean-up-output-of-linux-script-command (converted from perl to Python regex)
+META_CHAR_PATT = re.compile('\x1b([^\[\]]|\[.*?[a-zA-Z]|\].*?\x07)')
+
+
+def clean_string(input_string: str, keep_backspace: bool=False, keep_bell: bool=False) -> str:
+    # Remove terminal escape sequences
+    input_string = META_CHAR_PATT.sub('', input_string)
+    
+    # Source: https://stackoverflow.com/questions/36576216/apply-control-characters-to-a-string-python
+    cursor_position: int = 0
+    displayed_string: list = []
+    
+    # Apply any backspace sequences, otherwise output the original character
+    character: str
+    for character in input_string:
+        if character != '\x08' or keep_backspace:
+            # Add the character to the string
+            displayed_string[cursor_position:cursor_position+1] = character
+            
+            # Move the cursor forward
+            cursor_position += 1
+        else:
+            # Remove backspace
+            
+            # Move the cursor backward
+            cursor_position -= 1
+            
+            # Ignore undisplayed BELL characters when moving the cursor backwards
+            while input_string[cursor_position] == '\x07':
+                cursor_position -= 1
+    
+    # Remove BELL characters
+    if not keep_bell:
+        displayed_string = [c for c in displayed_string if c != '\x07']
+    
+    return ''.join(displayed_string[:cursor_position])
+
+
+def clean_file(input_filepath: str, output_filepath: str, keep_backspace: bool=False, keep_bell: bool=False) -> None:
+    print(f'[*] Cleaning: { input_filepath }...')
+    
+    with open(input_filepath, 'r', encoding='utf-8') as in_file:
+        with open(output_filepath, 'w') as out_file:
+            line: str
+            for line in in_file.readlines():
+                out_file.write(clean_string(line, keep_backspace, keep_bell))
+    
+    print(f'[+] Done! Output written to: { output_filepath }')
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Cleans shell typescript files by removing terminal escape sequences and (optionally) applying backspace characters')
+    parser.add_argument('--keep_backspace', action="store_true", dest='keep_backspace', help='Keep BACKSPACE characters; useful for identifying things typed accidentally (like passwords)')
+    parser.add_argument('--keep_bell', action="store_true", dest='keep_bell', help='Keep BELL characters; useful for identifying tab completion')
+    parser.add_argument('filepaths', nargs='*', help='Paths to typescript files (i.e., logs created using the "script" command)')
+    args = parser.parse_args()
+    
+    if len(args.filepaths) > 0:
+        input_filepath: str
+        for input_filepath in args.filepaths:
+            output_filepath: str
+            ext: str
+            output_filepath, ext = os.path.splitext(input_filepath)
+            output_filepath = f'{ output_filepath }_cleaned{ ext }'
+            
+            clean_file(input_filepath, output_filepath, args.keep_backspace, args.keep_bell)
+    else:
+        parser.print_help(sys.stderr)
